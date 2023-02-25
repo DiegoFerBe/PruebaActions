b8f523fa4829d77d64d5d692107ecdbeb2165fe4
Diego Fernando Becerra Zambrano <d.becerraz@uniandes.edu.co>
2023-02-24 20:21:25 -0500

    Caso 3: crear clave favorita (rojo)

--- src/logica/LogicaCaja.py
+++ src/logica/LogicaCaja.py
@@ -1,11 +1,14 @@
 class LogicaCaja:
 
-   def validar_clave_maestra(self,clave):
-       clave = 'clave'
-       if clave == 'clave':
-           return True
-       else:
-           return False
+    def validar_clave_maestra(self, clave):
+        clave = 'clave'
+        if clave == 'clave':
+            return True
+        else:
+            return False
 
-   def ver_claves_favoritas(self):
-       return []
+    def ver_claves_favoritas(self):
+        return []
+
+    def crear_clave_favorita(self, nombre, clave, confirmacion, pista):
+        return False
--- tests/TestLogicaCaja.py
+++ tests/TestLogicaCaja.py
@@ -1,12 +1,17 @@
 import unittest
 from src.logica.LogicaCaja import LogicaCaja
 
+
 class TestLogicaCaja(unittest.TestCase):
 
     def test_validar_clave_maestra(self):
-        clave=LogicaCaja.validar_clave_maestra(self,'clave')
-        self.assertEqual(clave,True)
+        clave = LogicaCaja.validar_clave_maestra(self, 'clave')
+        self.assertEqual(clave, True)
 
     def test_ver_claves_favoritas(self):
         clavesFavortias = LogicaCaja.ver_claves_favoritas(self)
-        self.assertEqual(clavesFavortias,[])
+        self.assertEqual(clavesFavortias, [])
+
+    def test_crear_clave_favorita(self):
+        crearClaveFavorita = LogicaCaja.crear_clave_favorita(self, 'prueba','erferfre','erferfre','la pista')
+        self.assertEqual(crearClaveFavorita, True)
