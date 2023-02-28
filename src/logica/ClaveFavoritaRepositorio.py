5dfb65f85a8543ffa49b73e19c84865a39fbf17a
Diego Fernando Becerra Zambrano <d.becerraz@uniandes.edu.co>
2023-02-27 20:22:43 -0500

    Repositorio para claves favoritas

---
+++ src/logica/ClaveFavoritaRepositorio.py
@@ -0,0 +1,28 @@
+from sqlalchemy import MetaData
+from sqlalchemy.exc import SQLAlchemyError
+
+from src.Modelo.declarative_base import engine, Base, session
+from src.Modelo.clavesFavoritas import ClavesFavoritas
+
+
+class ClaveFavoritaRepositorio:
+
+    def __init__(self):
+        Base.metadata.create_all(engine)
+        metadata = MetaData()
+        metadata.create_all(engine)
+
+    def guardar_clave_favorita(self, nombre, clave, pista):
+        try:
+            c = ClavesFavoritas(nombre=nombre, clave=clave, pista=pista)
+            session.add(c)
+            session.commit()
+            session.close()
+            return True
+        except SQLAlchemyError:
+            return False
+
+    def ver_claves_favoritas(self):
+        clavesFavoritas = session.query(ClavesFavoritas).all()
+        session.close()
+        return clavesFavoritas
