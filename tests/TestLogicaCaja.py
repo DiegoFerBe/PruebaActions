0b03691aa47d4c8ead19dfd0be50f727ad7e3c1a
Diego Fernando Becerra Zambrano <d.becerraz@uniandes.edu.co>
2023-03-12 15:49:55 -0500

    SOLUCIONADO test 1

--- tests/TestLogicaCaja.py
+++ tests/TestLogicaCaja.py
@@ -78,7 +78,7 @@ class TestLogicaCaja(unittest.TestCase):
 
         respuesta = self.logicCaja.editar_clave(1, nuevoNombre, nuevaClave, nuevaPista)
 
-        self.assertTrue(False, True)
+        self.assertTrue(respuesta, True)
 
     def tearDown(self):
         self.session = Session()
